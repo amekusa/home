80d53f854432c858bb6c8b09bb39f366cd4f5c18
amekusa <hello@amekusa.com>
2022-01-14 16:41:25 +0900

    Colorize update.sh

--- .sh/bin/update.sh
+++ .sh/bin/update.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+BASE="$HOME/.sh"
+source "$BASE/lib/styles.sh"
+
+REPOS_DIR="$BASE/repos"
 REPOS=(
   https://github.com/zsh-users/zsh-autosuggestions
   https://github.com/zsh-users/zsh-syntax-highlighting
@@ -9,9 +13,6 @@ REPOS=(
   https://github.com/tj/n
 )
 
-BASE_DIR="$HOME/.sh"
-REPOS_DIR="$BASE_DIR/repos"
-
 [ -d "$REPOS_DIR" ] || mkdir -p "$REPOS_DIR"
 cd "$REPOS_DIR" || exit 1
 
@@ -19,21 +20,19 @@ echo "Checking repositories..."
 
 for repo in "${REPOS[@]}"; do
   echo
-  echo "=> $repo.git"
+  echo -e "=> ${CYN}$repo.git${RESET}"
   dir=$(basename "$repo")
 
   if [ -d "$dir" ]; then
     echo " -> Found at $PWD/$dir"
     cd "$dir"
     echo " -> Updating..."
-    git pull origin master
-    echo " -> Done."
+    git pull origin master && echo -e " -> ${GRN}Done.${RESET}"
     cd ..
 
   else
     echo " -> Cloning into $PWD/$dir"...
-    git clone "$repo.git"
-    echo " -> Done."
+    git clone "$repo.git" && echo -e " -> ${GRN}Done.${RESET}"
   fi
 
 done
