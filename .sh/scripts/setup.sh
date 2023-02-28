6dec9e013ff843beedb8b6f6228eea8bd0171beb
amekusa <hello@amekusa.com>
2023-02-28 19:43:06 +0900

    [vscode] Symlink keybindings.json

--- .sh/scripts/setup.sh
+++ .sh/scripts/setup.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 BASE="$HOME/.sh"
+. "$BASE/shlib/io.sh"
 . "$BASE/shlib/format.sh"
 
 dir="$HOME/Library/Application Support/Code/User"
 if [ -d "$dir" ]; then
-	file="settings.json"
-	[ -f "$dir/$file" ] && rm "$dir/$file"
-	ln -s "$HOME/.vscode/$file" "$dir/$file" &&
-	echo -e "[${GRN}SUCCESS${RST}] Symlinked VSCode settings.json"
-
-	file="snippets"
-	[ -d "$dir/$file" ] && rm -rf "$dir/$file"
-	ln -s "$HOME/.vscode/$file" "$dir/$file" &&
-	echo -e "[${GRN}SUCCESS${RST}] Symlinked VSCode snippets"
+	files=(
+		"settings.json"
+		"keybindings.json"
+		"snippets"
+	)
+	for each in "${files[@]}"; do
+		src="$HOME/.vscode/$each"
+		_symlink -F "$src" "$dir/$each" &&
+		echo -e "[${GRN}SUCCESS${RST}] Symlinked VSCode $each"
+	done
 fi
 
 list=(
