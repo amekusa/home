aa662af474ccde570218217feae004d4b78ae552
amekusa <hello@amekusa.com>
2023-03-01 07:06:11 +0900

    [shell] Add script: nix-update.sh

---
+++ .sh/scripts/nix-update.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+#  Updates the default nixpkgs revision
+# -------------------------------------- -
+#  author: github.com/amekusa
+#  thanks: checkoway.net/musings/nix/
+# ============================================ *
+
+base="$HOME/.sh"
+. "$base/shlib/format.sh"
+
+echo "Fetching the latest revision via Prometheus API..."
+url='https://monitoring.nixos.org/prometheus/api/v1/query?query=channel_revision'
+filter='.data.result[]|select(.metric.status == "stable" and .metric.variant == "darwin").metric.revision'
+if ! rev=$(curl --silent --show-error "$url" | jq -r "$filter"); then
+	_error "failed to fetch the revision"; exit 1
+fi
+_success "revision: ${MAG}$rev${RST}"
+
+dir="$HOME/.nix-defexpr/nixpkgs"
+file="$dir/default.nix"
+data="import (fetchTarball \"https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz\")"
+
+[ -d "$dir" ] || mkdir -p "$dir"
+if [ -r "$file" ] && diff -q "$file" - <<< "$data" > /dev/null; then
+  echo "nixpkgs already up to date"
+  exit
+fi
+if ! echo "$data" > $file; then
+	_error "failed to write: $file"; exit 1
+fi
+_success "default nixpkgs has been updated"
+
+cat <<- EOF
+Update the packages with:
+  nix-env -irf <envfile.nix>
+
+EOF
