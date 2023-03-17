1d9d4c616da3a847f8a4df6ec2725b445c130735
amekusa <hello@amekusa.com>
2023-03-17 21:42:14 +0900

    [setup.sh] Use task system

--- .sh/scripts/setup.sh
+++ .sh/scripts/setup.sh
@@ -1,42 +1,62 @@
 #!/bin/bash
 BASE="$HOME/.sh"
+. "$BASE/shlib/util.sh"
 . "$BASE/shlib/io.sh"
 . "$BASE/shlib/format.sh"
+. "$BASE/shlib/task.sh"
+
+task-system --save-to "$BASE/.setup.tasks" "$@"
+
+# ---- npm ----
+if task NPM && _has-cmd npm; then
+	npm config set prefix "$NPM_PREFIX"
+	pkgs=(
+		"npm-check-updates"
+		"c8"
+		"mocha"
+		"gulp"
+		"jsdoc@3.6.11"
+	)
+	for each in "${pkgs[@]}"; do
+		npm install -g "$each"
+	done
+ksat; fi
+
 
 # ---- vscodium ----
+if task VSCODE; then
+	exe="/Applications/VSCodium.app/Contents/Resources/app/bin/codium"
+	dir="$HOME/Library/Application Support/VSCodium/User"
+	if [ -d "$dir" ]; then
+		files=(
+			"settings.json"
+			"keybindings.json"
+			"snippets"
+		)
+		for each in "${files[@]}"; do
+			src="$HOME/.vscode/$each"
+			_symlink -F "$src" "$dir/$each" &&
+			_success "Symlinked VSCode $each"
+		done
+	fi
+	extensions=(
+		# themes
+		"paulvandermeijs.loved"
+		"Holllo.love"
+		"qufiwefefwoyn.kanagawa"
+		"jdinhlife.gruvbox"
 
-exe="/Applications/VSCodium.app/Contents/Resources/app/bin/codium"
-dir="$HOME/Library/Application Support/VSCodium/User"
-if [ -d "$dir" ]; then
-	files=(
-		"settings.json"
-		"keybindings.json"
-		"snippets"
+		# utils
+		"EditorConfig.EditorConfig"
+		"marp-team.marp-vscode"
+		"alefragnani.Bookmarks"
+		"huntertran.auto-markdown-toc"
+		"wwm.better-align"
 	)
-	for each in "${files[@]}"; do
-		src="$HOME/.vscode/$each"
-		_symlink -F "$src" "$dir/$each" &&
-		echo -e "[${GRN}SUCCESS${RST}] Symlinked VSCode $each"
-	done
-fi
+	for each in "${extensions[@]}"; do
+		"$exe" --install-extension "$each" &&
+		_success "Installed VSCode extension '$each'"
+	done;
+ksat; fi
 
-extensions=(
-	# themes
-	"paulvandermeijs.loved"
-	"Holllo.love"
-	"qufiwefefwoyn.kanagawa"
-	"jdinhlife.gruvbox"
-
-	# utils
-	"EditorConfig.EditorConfig"
-	"marp-team.marp-vscode"
-	"alefragnani.Bookmarks"
-	"huntertran.auto-markdown-toc"
-	"wwm.better-align"
-)
-for each in "${extensions[@]}"; do
-	"$exe" --install-extension "$each" &&
-	echo -e "[${GRN}SUCCESS${RST}] Installed VSCode extension '$each'"
-done;
-
-echo -e "${GRN}Done.${RST}"
+_success "Done."
