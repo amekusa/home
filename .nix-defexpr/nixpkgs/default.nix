9ef29d2f925364eca48ca424f4965b43ea3251a9
amekusa <hello@amekusa.com>
2023-03-28 04:03:09 +0900

    [nix] Update nixpkgs

--- .nix-defexpr/nixpkgs/default.nix
+++ .nix-defexpr/nixpkgs/default.nix
@@ -1,1 +1,1 @@
-import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/da9e1809cd89f6d6a45a8c639436b9a6471ef004.tar.gz") # update: 2023-03-01
+import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/45d24f02e577c228a6170606c943ee97a9614619.tar.gz") # update: 2023-03-15
