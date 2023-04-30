f83dbd5ef010005c289206c42bc9595b76ee6cce
amekusa <hello@amekusa.com>
2023-04-30 15:52:06 +0900

    [nix] Bump nixpkgs

--- .nix-defexpr/nixpkgs/default.nix
+++ .nix-defexpr/nixpkgs/default.nix
@@ -1,1 +1,1 @@
-import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/47b0fe7da4ecaf1150abbc63aa4fb20bab00b430.tar.gz") # update: 2023-04-18
+import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/7c786944f801745310578d1cfc019923396f830c.tar.gz") # update: 2023-04-21
