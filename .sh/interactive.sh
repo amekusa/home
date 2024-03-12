7289a30c3e1844c6c39e9980a7f8a88a5788f7de
amekusa <hello@amekusa.com>
2024-03-13 02:44:39 +0900

    sh: add aliases

--- .sh/interactive.sh
+++ .sh/interactive.sh
@@ -27,6 +27,7 @@ alias lg='lazygit'
 
 # neovim
 alias vi='nvim'
+alias vim='nvim'
 alias vimdiff='nvim -d'
 
 # fix micro + tmux conflicts
@@ -40,6 +41,17 @@ alias nwatch='npm run watch'
 
 # VSCodium
 alias code='/Applications/VSCodium.app/Contents/Resources/app/bin/codium'
+
+# nnn
+alias n='nnn'
+
+# yazi
+function y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXX")"
+	yazi "$@" --cwd-file="$tmp"
+	cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && cd -- "$cwd"
+	rm -f -- "$tmp"
+}
 
 # start/stop server
 alias start-server='brew services run mariadb; php-fpm -D; sudo nginx'
