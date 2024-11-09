6ad074d1df58dfa66dff0a900a6ab6513d04b1e3
Satoshi Soma <noreply@amekusa.com>
2024-11-10 00:54:13 +0900

    scripts:ytdl: minor improvements

--- .sh/scripts/ytdl.sh
+++ .sh/scripts/ytdl.sh
@@ -1,22 +1,27 @@
 #!/usr/bin/env bash
-BASE="$HOME/.sh"
-. "$BASE/lib/ush/load" util
-. "$BASE/lib/ush/load" fmt; _ansi
+# ---------------------------------------------------------
+#  Download a YouTube video with the best quality of audio
+# ========================================================= *
+base="$HOME/.sh"
+. "$base/lib/ush/load" util
+. "$base/lib/ush/load" fmt; _ansi
+
+_chk-cmd ffmpeg
+ytdl="$(_fb-cmd yt-dlp youtube-dl)" || _die "yt-dlp or youtube-dl is required"
 
 # ---- config ----
-FORMAT="bestvideo[ext!=webm][height<=720]+bestaudio[ext!=webm]/bestvideo+bestaudio/best"
-OUTPUT="%(title)s #%(id)s"
-DESC_EXT=".desc.txt"
-OPTS="--ignore-config"
+format="bestvideo[ext!=webm][height<=720]+bestaudio[ext!=webm]/bestvideo+bestaudio/best"
+output="%(title)s #%(id)s"
+desc_ext=".desc.txt"
+opts="--ignore-config"
 # ================
 
-ytdl=$(which youtube-dl) || _die "youtube-dl required"
-
-title="$($ytdl $OPTS -s --get-title $@)"
-desc="$title$LF$LF$($ytdl $OPTS -s --get-description $@)"
-filename="$($ytdl $OPTS -s -o "$OUTPUT" --get-filename $@)"
-echo "$desc" > "$filename$DESC_EXT"
+title="$($ytdl $opts -s --get-title $@)"
+desc="$title$LF$LF$($ytdl $opts -s --get-description $@)"
+filename="$($ytdl $opts -s -o "$output" --get-filename $@)"
 echo "$desc"
 echo "-------- -"
 
-$ytdl $OPTS -f "$FORMAT" -o "$OUTPUT.%(ext)s" $@
+$ytdl $opts -f "$format" -o "$output.%(ext)s" $@ &&
+echo "$desc" > "$filename$desc_ext"
+
