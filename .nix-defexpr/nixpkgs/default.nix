4b86c69e32f14a5e49eb8bbd045418ae8dc47645
Satoshi Soma <noreply@amekusa.com>
2025-05-05 21:20:59 +0900

    nix: update

--- .nix-defexpr/nixpkgs/default.nix
+++ .nix-defexpr/nixpkgs/default.nix
@@ -1,1 +1,1 @@
-import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/c90283d00444be0b013825b9168306c592801265.tar.gz") # update: 2024-12-09
+import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/e8a44f32c46444d45482e6f962b52dc21ba91281.tar.gz") # update: 2025-03-24
